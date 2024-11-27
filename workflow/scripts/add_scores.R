5a0d13d16c6f24957effac49830b188ac9b2c222
Michael Piechotta <7385779+piechottam@users.noreply.github.com>
2024-11-27 22:59:24 +0100

    Update score max

--- workflow/scripts/add_scores.R
+++ workflow/scripts/add_scores.R
@@ -34,20 +34,20 @@ stopifnot(!is.null(opts$options$output))
 stopifnot(!is.null(opts$options$stat))
 stopifnot(length(opts$args) == 1)
 
-summarise_ratio <- function(r, f) {
+summarise_ratio <- function(r, get_ratios, f) {
   by_conds <- split(colData(r), colData(r)$condition) |>
     lapply(rownames)
   cols1 <- by_conds[[names(by_conds)[1]]]
   cols2 <- by_conds[[names(by_conds)[2]]]
 
-  ratios <- f(r)
+  ratios <- get_ratios(r)
   if (length(cols1) > 1) {
-    ratios1 <- rowMeans(ratios[, cols1])
+    ratios1 <- f(ratios[, cols1])
   } else {
     ratios1 <- ratios[, cols1]
   }
   if (length(cols2) > 1) {
-    ratios2 <- rowMeans(ratios[, cols2])
+    ratios2 <- f(ratios[, cols2])
   } else {
     ratios2 <- ratios[, cols2]
   }
@@ -56,22 +56,37 @@ summarise_ratio <- function(r, f) {
 }
 
 
-norm_score <- function(r, score) {
+norm_score <- function(r, score, score_sampled) {
   scores <- rowData(r)[[score]]
-  scores_subsampled <- rowData(r)[[paste0(score, "_subsampled")]]
+  scores_subsampled <- rowData(r)[[score_sampled]]
   runs <- lapply(scores_subsampled, length) |>
     unlist() |>
     unique()
-  stopifnot(length(runs) == 1 || runs == 0)
+  # stopifnot(length(runs) == 1 || runs == 0)
 
-  i <- !mapply(function(observed, subsampled) { return((sum(observed >= subsampled) / runs) >= 0.9) },
+  # i <- !mapply(function(observed, subsampled) { return((sum(observed >= subsampled) / runs) >= 0.9) },
+  #              as.list(scores), scores_subsampled)
+  new_scores <- mapply(function(observed, subsampled) { return(observed - max(subsampled, na.rm = TRUE)) },
                as.list(scores), scores_subsampled)
-  #i <- (scores <= lapply(scores_subsampled, median) |> unlist())
 
+  i <- new_scores < 0
   if (any(i)) {
-    scores[i] <- 0
+    new_scores[i] <- 0
   }
 
+  new_scores
+}
+
+summarise_score <- function(r, score, f) {
+  scores <- rowData(r)[[score]]
+  runs <- lapply(scores, length) |>
+    unlist() |>
+    unique()
+  stopifnot(length(runs) == 1 || runs == 0)
+  
+  scores <- lapply(scores, f) |>
+    unlist()
+  
   scores
 }
 
@@ -79,13 +94,25 @@ PARSE_COLS <- list("mismatch_score" = function(r) { return(rowData(r)$score) },
                    "insertion_score" = function(r) { return(rowData(r)$insertion_score) },
                    "deletion_score" = function(r) { return(rowData(r)$deletion_score) },
                    #
-                   "norm_mismatch_score" = function(r) { return(norm_score(r, "score")) },
-                   "norm_insertion_score" = function(r) { return(norm_score(r, "insertion_score")) },
-                   "norm_deletion_score" = function(r) { return(norm_score(r, "deletion_score")) },
+                   "norm_mismatch_score_subsampled" = function(r) { return(norm_score(r, "score", "score_subsampled")) },
+                   "norm_insertion_score_subsampled" = function(r) { return(norm_score(r, "insertion_score", "insertion_score_subsampled")) },
+                   "norm_deletion_score_subsampled" = function(r) { return(norm_score(r, "deletion_score", "deletion_score_subsampled")) },
                    #
-                   "non_ref_ratio" = function(r) { return(summarise_ratio(r, non_ref_ratio)) },
-                   "insertion_ratio" = function(r) { return(summarise_ratio(r, insertion_ratio)) } ,
-                   "deletion_ratio" = function(r) { return(summarise_ratio(r, deletion_ratio)) })
+                   "norm_mismatch_score_downsampled" = function(r) { return(norm_score(r, "score", "score_downsampled")) },
+                   "norm_insertion_score_downsampled" = function(r) { return(norm_score(r, "insertion_score", "insertion_score_downsampled")) },
+                   "norm_deletion_score_subsampled" = function(r) { return(norm_score(r, "deletion_score", "deletion_score_downsampled")) },
+                   #
+                   "median_mismatch_score_downsampled" = function(r) { return(summarise_score(r, "score_downsampled", median)) },
+                   "median_insertion_score_downsampled" = function(r) { return(summarise_score(r, "insertion_score_downsampled", median)) },
+                   "median_deletion_score_downsampled" = function(r) { return(summarise_score(r, "deletion_score_downsampled", median)) },
+                   #
+                   "mean_mismatch_score_downsampled" = function(r) { return(summarise_score(r, "score_downsampled", mean)) },
+                   "mean_insertion_score_downsampled" = function(r) { return(summarise_score(r, "insertion_score_downsampled", mean)) },
+                   "mean_deletion_score_downsampled" = function(r) { return(summarise_score(r, "deletion_score_downsampled", mean)) },
+                   #
+                   "mean_non_ref_ratio" = function(r) { return(summarise_ratio(r, non_ref_ratio, rowMeans)) },
+                   "mean_insertion_ratio" = function(r) { return(summarise_ratio(r, insertion_ratio, rowMeans)) } ,
+                   "mean_deletion_ratio" = function(r) { return(summarise_ratio(r, deletion_ratio, rowMeans)) })
 
 clean_score <- function(score) {
   tidyr::replace_na(score, 0)
@@ -98,7 +125,6 @@ parse_stats <- function(stat_opts) {
   df <- do.call(rbind, l) |>
     as.data.frame()
   colnames(df) <- c("new_col", "stat")
-  df[, "stat"] <- gsub("norm:", "norm_", df[, "stat"])
 
   df
 }
@@ -128,8 +154,7 @@ parse_result <- function(r, stats) {
   cols <- c("ref", "ref_context")
   df <- rowRanges(r)[, c("ref", "ref_context")] |>
     as.data.frame() |>
-    dplyr::rename(trna = seqnames,
-                  pos = start) |>
+    dplyr::rename(trna = seqnames, pos = start) |>
     dplyr::select(trna, pos, strand, ref, ref_context)
 
   # add intermediate columns
@@ -139,9 +164,19 @@ parse_result <- function(r, stats) {
   for (col in pick_cols(stats$stat)) {
     if (!col %in% colnames(df)) {
       f <- PARSE_COLS[[col]]
+      if (is.null(f)) {
+        print(col)
+      }
       df[, col] <- f(r)
     }
   }
+
+  # score_cols <- c("score", "score_subsampled", "score_downsampled",
+  #                 "deletion_score", "deletion_score_subsampled", "deletion_score_downsampled",
+  #                 "insertion_score", "insertion_score_subsampled", "insertion_score_downsampled")
+  #for (col in score_cols) {
+  #  df[, col] <- rowRanges(r)[, col]
+  #}
 
   # add final columns
   for (i in rownames(stats)) {
@@ -151,6 +186,7 @@ parse_result <- function(r, stats) {
       df[, new_col] <- with(df, eval(parse(text = stat)))
     }
   }
+
   # keep what is needed
   cols <- c("trna", "pos", "strand", cols, stats$new_col)
   df <- df[, cols]
@@ -161,7 +197,8 @@ parse_result <- function(r, stats) {
 res_to_r <- function(res) {
   rowRanges <- res[, c("score", "filter", "ref",
                        "insertion_score", "deletion_score",
-                       "score_subsampled", "deletion_score_subsampled", "insertion_score_subsampled")]
+                       "score_subsampled", "deletion_score_subsampled", "insertion_score_subsampled",
+                       "score_downsampled", "deletion_score_downsampled", "insertion_score_downsampled")]
   mcols(rowRanges)[, "ref_context"] <- "N"
 
   assays <- list()
@@ -183,18 +220,14 @@ res_to_r <- function(res) {
   colData <- data.frame(condition = condition)
   row.names(colData) <- names(assays[["bases"]])
 
-  # parse indels
-  #for (k in c("insertion", "deletion")) {
-  #  if (any(grepl(paste0("^", k), colnames(unpacked_info)))) {
-  #    res <- .add_indels(k, unpacked_info, assays, rowRanges, rownames(colData))
-  #    rowRanges <- res$rowRanges
-  #    assays <- res$assays
-  #  }
-  #}
-  for (score in c("score_subsampled", "insertion_score_subsampled", "deletion_score_subsampled")) {
+  scores <- c("score", "insertion_score", "deletion_score")
+  scores <- c(paste0(scores, "_subsampled"),
+              paste0(scores, "_downsampled"))
+  for (score in scores) {
     mcols(rowRanges)[[score]] <- strsplit(mcols(rowRanges)[[score]], ",") |>
         lapply(as.numeric) |>
-        lapply(unlist)
+        lapply(unlist) |>
+        clean_score()
   }
 
   l <- strsplit(res$reads, split = ",", fixed = TRUE) |>
@@ -216,10 +249,11 @@ res_to_r <- function(res) {
 
 # load JACUSA2 output and add... reference context
 res <- JACUSA2helper::read_result(opts$args,
-                                unpack = c("score_subsampled",
+                                unpack = c("score_subsampled", "score_downsampled",
                                            "reads",
                                            "insertion_score", "deletion_score",
-                                           "insertion_score_subsampled", "deletion_score_subsampled"))
+                                           "insertion_score_subsampled", "deletion_score_subsampled",
+                                           "deletion_score_downsampled", "insertion_score_downsampled"))
 r <- res_to_r(res)
 
 # parse options and create output container
@@ -231,6 +265,6 @@ df$Pos3 <- df$pos
 
 write.table(df,
             opts$options$output,
-            quote=FALSE,
+            quote = FALSE,
             sep = "\t",
             col.names = TRUE, row.names = FALSE)
